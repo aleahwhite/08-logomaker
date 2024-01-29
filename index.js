20c69e74c747e2c7dfb0fb2803631a01be91f529
Aleah White <aleahwhite55@gmail.com>
2024-01-29 15:59:06 -0600

    array of questions, classes, functions

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+import inquirer from "inquirer";
+import fs from 'fs';
+import {Circle, Square, Triangle} from './lib/shapes.js';
 
+class CreateSVG {
+    constructor() {
+        this.textElement = '';
+        this.shapeElement = '';
+    }
+    render() {
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text, textColor) {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`
+    }
+    setShapeElement(shape) {
+        this.shapeElement = shape.render()
+    } 
+}
 
 const questions = [
     {
@@ -23,3 +41,46 @@ const questions = [
         message: 'What color would you like your shape color to be?'
     }
 ];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err){
+        if (err) {
+            return console.log('Error generating logo:(', err);
+        }
+        console.log('Your SVG logo has been generated!');
+    });
+}
+
+async function init() {
+    try {
+        const answers = await inquirer.prompt(questions);
+
+        const svg = new CreateSVG();
+        svg.setTextElement(answers.text, answers.textColor);
+
+        let shape;
+        switch (answers.shape) {
+            case 'Circle':
+                shape = new Circle();
+                break;
+            case 'Square':
+                shape = new Square();
+                break;
+            case 'Triangle':
+                shape = new Triangle();
+                break;
+        }
+
+        shape.setColor(answers.shapeColor);
+        svg.setShapeElement(shape);
+
+        const svgData = svg.render();
+
+        const fileName = "logo.svg";
+        writeToFile(fileName, svgData);
+    } catch (error) {
+        console.error('An error occurred: ', error);
+    }
+}
+
+init();
